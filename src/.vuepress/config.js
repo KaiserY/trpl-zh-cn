84b66e80f51b97df26af1d7f983dd96746b7230c
singcl <1276837689@qq.com>
2022-09-20 23:53:36 +0800

    feat(core): vuepress build env

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,9 +1,13 @@
+import path from 'path';
 // .vuepress/config.js
-module.exports = {
+export default {
   themeConfig: {
     sidebar: "auto",
     nav: [
       { text: '回首页', link: '/' },
     ]
-  }
+  },
+  alias: {
+    'img': path.resolve(__dirname, './../img'),
+  },
 };
