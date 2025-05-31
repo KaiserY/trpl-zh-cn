df1083ae968c52a1eb52a6d6a2762405dcc7f585
须语 <Rainanxu@foxmail.com>
2025-05-31 17:17:28 +0800

    修复19-9的代码错误。

--- listings/ch19-patterns-and-matching/listing-19-09/src/main.rs
+++ listings/ch19-patterns-and-matching/listing-19-09/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let some_option_value: Option<i32> = None;
     // ANCHOR: here
-    let Some(x) = some_option_value else {
+    if let Some(x) = some_option_value else {
         return;
     };
     // ANCHOR_END: here
