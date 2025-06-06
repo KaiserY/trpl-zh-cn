f7da8ed2d6e8c6dc84039e08b6f17ba909c7d431
kazeno <kazeno00001@gmail.com>
2025-06-06 09:42:55 +0800

    fix autocorrect

--- listings/ch04-understanding-ownership/listing-04-04/src/main.rs
+++ listings/ch04-understanding-ownership/listing-04-04/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let s1 = gives_ownership();        // gives_ownership 将它的返回值传递给s1
+    let s1 = gives_ownership();        // gives_ownership 将它的返回值传递给 s1
 
     let s2 = String::from("hello");    // s2 进入作用域
 
-    let s3 = takes_and_gives_back(s2); // s2 被传入takes_and_gives_back, 
+    let s3 = takes_and_gives_back(s2); // s2 被传入 takes_and_gives_back, 
                                        // 它的返回值又传递给 s3
-} // 此处，s3 移出作用域并被丢弃。s2 被 move ，所以无事发生
+} // 此处，s3 移出作用域并被丢弃。s2 被 move，所以无事发生
   // s1 移出作用域并被丢弃
 
 fn gives_ownership() -> String {       // gives_ownership 将会把返回值传入
