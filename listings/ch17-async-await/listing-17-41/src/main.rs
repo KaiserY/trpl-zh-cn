90d44058131f623330294ca509c0f06c7d0a5841
qwer252 <wq33392@gmail.com>
2025-06-10 23:55:17 +0800

    Update main.rs

--- listings/ch17-async-await/listing-17-41/src/main.rs
+++ listings/ch17-async-await/listing-17-41/src/main.rs
@@ -47,11 +47,11 @@ fn get_messages() -> impl Stream<Item = String> {
 fn get_intervals() -> impl Stream<Item = u32> {
     let (tx, rx) = trpl::channel();
 
-    // This is *not* `trpl::spawn` but `std::thread::spawn`!
+    // 这里*不是* `trpl::spawn` ，是 `std::thread::spawn`!
     thread::spawn(move || {
         let mut count = 0;
         loop {
-            // Likewise, this is *not* `trpl::sleep` but `std::thread::sleep`!
+            // 同样, 这里*不是* `trpl::sleep` ，是 `std::thread::sleep`!
             thread::sleep(Duration::from_millis(1));
             count += 1;
 
